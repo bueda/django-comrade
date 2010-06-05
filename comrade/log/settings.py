b25262c4620e344e89432754851bb1e6306c7c92
Christopher Peplin <peplin@bueda.com>
2010-06-05 19:46:38 -0400

    Add log settings.

---
+++ comrade/log/settings.py
@@ -0,0 +1,80 @@
+import logging, logging.handlers
+import types
+
+from django.conf import settings
+
+import commonware.log
+import dictconfig
+
+class UTF8SafeFormatter(commonware.log.Formatter):
+  def __init__(self, fmt=None, datefmt=None, encoding='utf-8'):
+    logging.Formatter.__init__(self, fmt, datefmt)
+    self.encoding = encoding
+ 
+  def formatException(self, e):
+    r = logging.Formatter.formatException(self, e)
+    if type(r) in [types.StringType]:
+      r = r.decode(self.encoding, 'replace') # Convert to unicode
+    return r
+ 
+  def format(self, record):
+    t = commonware.log.Formatter.format(self, record)
+    if type(t) in [types.UnicodeType]:
+      t = t.encode(self.encoding, 'replace')
+    return t
+
+class NullHandler(logging.Handler):
+    def emit(self, record):
+        pass
+
+base_fmt = ('%(name)s:%(levelname)s %(message)s:%(pathname)s:%(lineno)s')
+
+cfg = {
+    'version': 1,
+    'filters': {},
+    'formatters': {
+        'debug': {
+            '()': UTF8SafeFormatter,
+            'datefmt': '%H:%M:%s',
+            'format': '%(asctime)s ' + base_fmt,
+        },
+        'prod': {
+            '()': UTF8SafeFormatter,
+            'datefmt': '%H:%M:%s',
+            'format': '%s: [%%(REMOTE_ADDR)s] %s' % (settings.SYSLOG_TAG,
+                                                     base_fmt),
+        },
+    },
+    'handlers': {
+        'console': {
+            '()': logging.StreamHandler,
+            'formatter': 'debug',
+        },
+        'syslog': {
+            '()': logging.handlers.SysLogHandler,
+            'facility': logging.handlers.SysLogHandler.LOG_LOCAL0,
+            'address': '/dev/log',
+            'formatter': 'prod',
+        },
+        'null': {
+            '()': NullHandler,
+        },
+    },
+    'loggers': {
+    },
+    'root': {},
+}
+
+for key, value in settings.LOGGING.items():
+    cfg[key].update(value)
+
+# Set the level and handlers for all loggers.
+for logger in cfg['loggers'].values() + [cfg['root']]:
+    if 'handlers' not in logger:
+        logger['handlers'] = ['syslog' if settings.USE_SYSLOG else 'console']
+    if 'level' not in logger:
+        logger['level'] = settings.LOG_LEVEL
+    if logger is not cfg['root'] and 'propagate' not in logger:
+        logger['propagate'] = False
+
+dictconfig.dictConfig(cfg)
