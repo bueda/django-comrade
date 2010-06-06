a21b97f5227b504a6629d69cebf13fb46673a0f4
Christopher Peplin <peplin@bueda.com>
2010-06-05 21:49:48 -0400

    Pull out package deploy code to fab_shared.

--- fabfile.py
+++ fabfile.py
@@ -3,30 +3,17 @@ import os
 from fabric.api import *
 from fabric.contrib.console import confirm
 
-from fab_shared import (_clone, _make_release, _nose_test,
-        _conditional_upload_to_s3, TIME_NOW)
+from fab_shared import (_clone, _make_release, _nose_test, _upload_to_s3,
+        TIME_NOW, _package_deploy)
 
 env.unit = "comrade"
 env.scm = "git@github.com:bueda/django-comrade" % env
 env.root_dir = os.path.abspath(os.path.dirname(__file__))
+env.allow_no_tag = True
+env.upload_to_s3 = True
  
-def deploy(release=None):
-    """
-    Deploy a specific commit, tag or HEAD to all servers and/or S3.
-    """
-    require('unit')
-
-    env.scratch_path = '/tmp/%s-%s' % (env.unit, TIME_NOW)
-    _clone(release)
-    if test(env.scratch_path):
-        abort("Unit tests did not pass")
-    _make_release(release)
-    require('pretty_release')
-    require('archive')
-
-    s3_filename = '%(unit)s.tar.gz' % env
-    s3_file_source = '%(scratch_path)s/%(archive)s' % env
-    _conditional_upload_to_s3(s3_file_source, s3_filename)
+def deploy(release=None, skip_tests=None):
+    _package_deploy(release, skip_tests)
 
 @runs_once
 def test(dir=None):
