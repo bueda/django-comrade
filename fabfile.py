dd972c530d0bdfedd07dc25c8f8da09c05834f00
Christopher Peplin <peplin@bueda.com>
2010-06-07 17:48:02 -0400

    Simplify fabfile.

--- fabfile.py
+++ fabfile.py
@@ -3,14 +3,12 @@ import os
 from fabric.api import *
 from fabric.contrib.console import confirm
 
-from fab_shared import _nose_test, _test, _package_deploy as deploy
+from fab_shared import (_nose_test_runner, _test as test,
+        _package_deploy as deploy)
 
 env.unit = "django-comrade"
 env.root_dir = os.path.abspath(os.path.dirname(__file__))
 env.scm = env.root_dir
 env.allow_no_tag = True
 env.upload_to_s3 = True
- 
-@runs_once
-def test(dir=None):
-    _test(_nose_test, dir)
+env.test_runner = _nose_test_runner
