0534c1cdeb92503a90ef309dee6edddb45234bf7
Christopher Peplin <peplin@bueda.com>
2010-06-16 16:25:47 -0400

    Resolve old Django 1.1 bug in URLs to keep it DRY.

--- comrade/users/urls.py
+++ comrade/users/urls.py
@@ -1,18 +1,21 @@
 from django.conf.urls.defaults import *
+from django.core.urlresolvers import reverse
+from django.utils.functional import lazy
+
+reverse_lazy = lazy(reverse, unicode)
 
 urlpatterns = patterns('django.contrib.auth.views',
     url(r'^login/', 'login', name='login'),
     url(r'^logout/', 'logout', {'next_page':'/'}, name='logout'),
     url(r'^password/forgot/$', 'password_reset',
-            # LH #269 - ideally this wouldn't be hard coded
-            {'post_reset_redirect':'/accounts/password/forgot/done/'},
+            {'post_reset_redirect':reverse_lazy('users:password_reset_done')},
             name='password_reset'),
     url(r'^password/forgot/done/$', 'password_reset_done',
             name='password_reset_done'),
     url(r'^password/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$',
             'password_reset_confirm',
-            # LH #269
-            {'post_reset_redirect':'/accounts/password/reset/done/'},
+            {'post_reset_redirect':
+                reverse_lazy('users:password_reset_complete')},
             name='password_reset_confirm'),
     url(r'^password/reset/done/$', 'password_reset_complete',
             name='password_reset_complete'),
