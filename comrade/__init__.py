837810b5f13327562849a69f538e424d3874410d
Christopher Peplin <peplin@bueda.com>
2010-06-16 12:28:01 -0400

    Pull ADMINS tuple out to comrade.

---
+++ comrade/__init__.py
@@ -0,0 +1,6 @@
+ADMINS = (
+           ('Christopher Peplin', 'peplin@bueda.com'),
+           ('Vasco Pedro', 'vasco@bueda.com'),
+           ('Vignesh Murugesan','vignesh@bueda.com'),
+           ('Ken Kochis', 'ken@bueda.com'),
+)
