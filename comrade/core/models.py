6bde05ad9c16be2677ab6c91444e793529a9fdd3
Christopher Peplin <peplin@bueda.com>
2010-06-18 18:34:07 -0400

    Add a base Django model to share common functionality.

---
+++ comrade/core/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class ComradeBaseModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.__unicode__()
+
+    def __repr__(self):
+        return self.__unicode__()
+
+    class Meta:
+        abstract = True
+        get_latest_by = 'created'
+
+    def get_absolute_url(self, *args, **kwargs):
+        return self.get_url_path(*args, **kwargs)
