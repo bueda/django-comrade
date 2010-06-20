57f09cf33c95a6abfbe3550962edcb1dd1aee6cc
Christopher Peplin <peplin@bueda.com>
2010-06-20 16:34:32 -0400

    Add many common app settings.

--- comrade/__init__.py
+++
@@ -1,6 +0,0 @@
-ADMINS = (
-           ('Christopher Peplin', 'peplin@bueda.com'),
-           ('Vasco Pedro', 'vasco@bueda.com'),
-           ('Vignesh Murugesan','vignesh@bueda.com'),
-           ('Ken Kochis', 'ken@bueda.com'),
-)
