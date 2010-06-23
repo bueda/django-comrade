9c247fabe361b217a553d45e6cd666863d4e5702
Christopher Peplin <peplin@bueda.com>
2010-06-23 09:59:54 -0400

    Add middleware to use all HTTP verbs in forms.

---
+++ comrade/core/middleware.py
@@ -0,0 +1,37 @@
+import re
+import itertools
+
+_HTML_TYPES = ('text/html', 'application/xhtml+xml')    
+
+_SUPPORTED_TRANSFORMS = ['PUT', 'DELETE']
+
+_FORM_RE = re.compile(r'((<form\W[^>]*\bmethod=(\'|"|))(%s)((\'|"|)\b[^>]*>))'
+        % '|'.join(_SUPPORTED_TRANSFORMS), re.IGNORECASE)
+
+_MIDDLEWARE_KEY = '_method'
+ 
+class HttpMethodsMiddleware(object):
+    def process_request(self, request):
+        if request.POST and request.POST.has_key(_MIDDLEWARE_KEY):
+            if request.POST[_MIDDLEWARE_KEY].upper() in _SUPPORTED_TRANSFORMS:
+                request.method = request.POST[_MIDDLEWARE_KEY].upper()
+        return None
+           
+    def process_response(self, request, response):
+        if response['Content-Type'].split(';')[0] in _HTML_TYPES:
+            # ensure we don't add the 'id' attribute twice (HTML validity)
+            idattributes = itertools.chain(("id='" + _MIDDLEWARE_KEY + "'",), 
+                                            itertools.repeat(''))
+            def add_transform_field(match):
+                """Returns the matched <form> tag with a modified method and
+                the added <input> element"""
+                return match.group(2) + "POST" + match.group(5) + \
+                "<div style='display:none;'>" + \
+                "<input type='hidden' " + idattributes.next() + \
+                " name='" + _MIDDLEWARE_KEY + "' value='" + \
+                match.group(4).upper() + "' /></div>"
+
+            # Modify any POST forms
+            response.content = _FORM_RE.sub(add_transform_field,
+                    response.content)
+        return response
