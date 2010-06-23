7f8d4a093648732b9ad0de31d98238e2d41d7d3d
Christopher Peplin <peplin@bueda.com>
2010-06-23 11:29:42 -0400

    Render instead of redirect on authorized access.

--- comrade/core/decorators.py
+++ comrade/core/decorators.py
@@ -1,3 +1,10 @@
+from django.utils.decorators import available_attrs
+from django.utils.http import urlquote
+from django.http import HttpResponse
+from django.template import loader, RequestContext
+
+from functools import wraps
+
 def singleton(cls):
     instances = {}
     def getinstance():
@@ -6,22 +13,20 @@ def singleton(cls):
         return instances[cls]
     return getinstance
 
-def authorized(test_func, unauthorized_url=None):
+def authorized(test_func, template_name='401.html'):
     """
     Decorator for views that checks that the user passes the given test,
     redirecting to the unauthorized page if it fails. The test should be a
     callable that takes the user object and returns True if the user passes.
     """
-    if not unauthorized_url:
-        from django.conf import settings
-        unauthorized_url = settings.UNAUTHORIZED_URL
 
     def decorator(view_func):
         def _wrapped_view(request, *args, **kwargs):
-            if test_func(request.user):
+            if test_func(request.user, *args, **kwargs):
                 return view_func(request, *args, **kwargs)
             path = urlquote(request.get_full_path())
-            return HttpResponseRedirect(unauthorized_url, status=401)
+            t = loader.get_template(template_name)
+            return HttpResponse(t.render(RequestContext(request)), status=401)
         return wraps(view_func,
                 assigned=available_attrs(view_func))(_wrapped_view)
     return decorator
