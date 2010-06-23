54b315be5db51e870ffbd96f1a16e0e4fb37ecd1
Christopher Peplin <peplin@bueda.com>
2010-06-23 15:41:30 -0400

    Coerce PUT data into a usable format.

--- comrade/core/router.py
+++ comrade/core/router.py
@@ -1,6 +1,43 @@
 from django.http import Http404, HttpResponseNotAllowed
 
-def get_handler_method(request_handler, http_method):
+def _coerce_put_post(request):
+    """
+    Django doesn't particularly understand REST.
+    In case we send data over PUT, Django won't
+    actually look at the data and load it. We need
+    to twist its arm here.
+    
+    The try/except abominiation here is due to a bug
+    in mod_python. This should fix it.
+    """
+    if request.method == "PUT":
+        # Bug fix: if _load_post_and_files has already been called, for
+        # example by middleware accessing request.POST, the below code to
+        # pretend the request is a POST instead of a PUT will be too late
+        # to make a difference. Also calling _load_post_and_files will result 
+        # in the following exception:
+        #   AttributeError: You cannot set the upload handlers after the upload
+        #     has been processed.
+        # The fix is to check for the presence of the _post field which is set 
+        # the first time _load_post_and_files is called (both by wsgi.py and 
+        # modpython.py). If it's set, the request has to be 'reset' to redo
+        # the query value parsing in POST mode.
+        if hasattr(request, '_post'):
+            del request._post
+            del request._files
+
+        try:
+            request.method = "POST"
+            request._load_post_and_files()
+            request.method = "PUT"
+        except AttributeError:
+            request.META['REQUEST_METHOD'] = 'POST'
+            request._load_post_and_files()
+            request.META['REQUEST_METHOD'] = 'PUT'
+            
+        request.PUT = request.POST
+
+def _get_handler_method(request_handler, http_method):
     try:
         handler_method = getattr(request_handler, http_method.lower())
         if callable(handler_method):
@@ -15,12 +52,15 @@ class RestfulResource:
     def dispatch(cls, request, *args, **kwargs):
         request_handler = cls()
 
+        _coerce_put_post(request)
+
         if request.method in cls.http_methods:
-            handler_method = get_handler_method(request_handler, request.method)
+            handler_method = _get_handler_method(request_handler, request.method)
             if handler_method:
                 return handler_method(request, *args, **kwargs)
 
-        methods = [method for method in cls.http_methods if get_handler_method(request_handler, method)]
+        methods = [method for method in cls.http_methods if
+                _get_handler_method(request_handler, method)]
         if len(methods) > 0:
             return HttpResponseNotAllowed(methods)
         else:
