1077d71858ed94968f04a643d43d83d6933f0e61
Christopher Peplin <peplin@bueda.com>
2010-06-24 09:56:15 -0400

    Add more intelligent JSON serialization (pulled from Piston).

--- comrade/http.py
+++ comrade/http.py
@@ -2,13 +2,98 @@ from django.core.serializers import json, serialize
 from django.db.models.query import QuerySet
 from django.http import HttpResponse
 from django.utils import simplejson
+from django.utils.encoding import smart_unicode
+from django.db.models import Model
+
+import decimal, inspect
 
 class HttpJsonResponse(HttpResponse):
     def __init__(self, object, status=None):
-        if isinstance(object, QuerySet):
-            content = serialize('json', object)
-        else:
-            content = simplejson.dumps(object, cls=json.DjangoJSONEncoder,
-                ensure_ascii=False)
+        content = simplejson.dumps(self._construct(object),
+                cls=json.DjangoJSONEncoder, ensure_ascii=False)
         super(HttpJsonResponse, self).__init__(
                 content, content_type='application/json')
+
+    @staticmethod
+    def _construct(data):
+        """
+        Recursively serialize a lot of types, and
+        in cases where it doesn't recognize the type,
+        it will fall back to Django's `smart_unicode`.
+
+        Returns `dict`.
+
+        Borrowed from the Piston API framework.
+        TODO if we use this a lot, consider just using the Piston framework
+        to do those tasks.
+        """
+        def _any(thing, fields=()):
+            """
+            Dispatch, all types are routed through here.
+            """
+            ret = None
+
+            if isinstance(thing, QuerySet):
+                ret = _qs(thing, fields=fields)
+            elif isinstance(thing, (tuple, list)):
+                ret = _list(thing)
+            elif isinstance(thing, dict):
+                ret = _dict(thing)
+            elif isinstance(thing, decimal.Decimal):
+                ret = str(thing)
+            elif isinstance(thing, Model):
+                ret = _model(thing, fields=fields)
+            elif inspect.isfunction(thing):
+                if not inspect.getargspec(thing)[0]:
+                    ret = _any(thing())
+            elif hasattr(thing, '__emittable__'):
+                f = thing.__emittable__
+                if inspect.ismethod(f) and len(inspect.getargspec(f)[0]) == 1:
+                    ret = _any(f())
+            elif repr(thing).startswith("<django.db.models.fields.related.RelatedManager"):
+                ret = _any(thing.all())
+            else:
+                ret = smart_unicode(thing, strings_only=True)
+
+            return ret
+
+        def _fk(data, field):
+            """
+            Foreign keys.
+            """
+            return _any(getattr(data, field.name))
+        
+        def _related(data, fields=()):
+            """
+            Foreign keys.
+            """
+            return [ _model(m, fields) for m in data.iterator() ]
+        
+        def _m2m(data, field, fields=()):
+            """
+            Many to many (re-route to `_model`.)
+            """
+            return [ _model(m, fields) for m in getattr(data, field.name).iterator() ]
+        
+        def _model(data, fields=()):
+            return _any(data.__dict__)
+        
+        def _qs(data, fields=()):
+            """
+            Querysets.
+            """
+            return serialize('json', data)
+                
+        def _list(data):
+            """
+            Lists.
+            """
+            return [ _any(v) for v in data ]
+            
+        def _dict(data):
+            """
+            Dictionaries.
+            """
+            return dict([ (k, _any(v)) for k, v in data.iteritems() ])
+            
+        return _any(data)
