ae3b624e4cf5116c0e07def40d90fcf40e381663
Christopher Peplin <peplin@bueda.com>
2010-07-15 18:43:18 -0400

    Test the new direct_to_email, clean it up.

--- comrade/mail/mail.py
+++ comrade/mail/mail.py
@@ -1,20 +1,22 @@
 from django.conf import settings
 from django.core.mail import EmailMessage
-from django.template import RequestContext
+from django.template import Context
 from django.template.loader import render_to_string
+from django.contrib.sites.models import Site
 
-def direct_to_email(template, recipient_list, form_email=None,
-        extra_context=None, subject_template=None):
+def direct_to_email(template, recipient_list, extra_context=None,
+        from_email=None, subject_template=None):
     """
     Sends an email of type email to user.email. Text of subject and message
-    rendered from templates/mail/{email}.txt and templates/mail/{email}_subject.txt
-    usign specified context
+    rendered from the {template} and {template}_subject using specified context
     """
     extra_context = extra_context or {}
-    context = RequestContext(extra_context)
+    context = Context(extra_context)
+    context['site'] = Site.objects.get_current()
     if not subject_template:
-        subject = render_to_string('mail/%s_subject.txt' % template, context)
-    body = render_to_string('mail/%s.txt' % template, context)
+        subject = render_to_string(
+                '%s_subject.txt' % template, context).strip('\n')
+    message = render_to_string('%s.txt' % template, context)
 
-    return EmailMessage(subject, body, recipient_list,
+    return EmailMessage(subject, message, from_email, recipient_list,
             headers={'Reply-To': settings.DEFAULT_FROM_EMAIL}).send()
