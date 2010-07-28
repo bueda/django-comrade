5c55243f6dc9ba410b7e86fef90a557ecada112c
Christopher Peplin <peplin@bueda.com>
2010-07-27 20:21:08 -0400

    Give PEP8 style names to Exception classes.

--- comrade/piston/resources.py
+++ comrade/piston/resources.py
@@ -1,15 +1,15 @@
 from piston.resource import Resource as PistonResource
 from django.http import HttpResponseServerError, HttpResponseBadRequest
 
-from comrade.exceptions import (ComradeException, HttpResponseException, 
-        BadRequestException)
+from comrade.exceptions import (ComradeError, HttpResponseError, 
+        BadRequestError)
 
 import commonware.log
 logger = commonware.log.getLogger('comrade.piston.resources')
 
 class Resource(PistonResource):
     def error_handler(self, error, request, method):
-        if isinstance(error, ComradeException):
+        if isinstance(error, ComradeError):
             logger.error(u'%s' % error)
             return _handle_comrade_exception(error)
         logger.exception(u'%s' % error)
@@ -20,7 +20,7 @@ class DebugResource(PistonResource):
         logger.exception(u'%s' % error)
         if 'text/html' in request.META.get('HTTP_ACCEPT', ''):
             raise
-        elif isinstance(error, ComradeException):
+        elif isinstance(error, ComradeError):
             return _handle_comrade_exception(error)
         else:
             error.name = 'Unhandled exception'
@@ -35,10 +35,10 @@ def _json_error(error):
     return response
 
 def _handle_comrade_exception(error):
-    if isinstance(error, HttpResponseException):
+    if isinstance(error, HttpResponseError):
         return HttpResponseServerError(
                 content=_json_error(error))
-    elif isinstance(error, BadRequestException):
+    elif isinstance(error, BadRequestError):
         return HttpResponseBadRequest(
                 content=_json_error(error))
     else:
