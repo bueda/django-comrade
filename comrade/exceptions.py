1a6e8160e9239b240236e09bac67ff6a913d76e2
Christopher Peplin <peplin@bueda.com>
2010-07-27 20:21:08 -0400

    Give PEP8 style names to Exception classes.

--- comrade/exceptions.py
+++ comrade/exceptions.py
@@ -1,17 +1,17 @@
 """
 Custom Python exceptions.
 
-Any exception inheriting from ComradeException will be caught by the API error
+Any exception inheriting from ComradeError will be caught by the API error
 handler and returned as HttpResponseForbidden or another specific error. Any
-exception not in the ComradeException hierarchy will result in an Internal
+exception not in the ComradeError hierarchy will result in an Internal
 Server Error.
 """
 
 from django.conf import settings
 
-class ComradeException(Exception):
+class ComradeError(Exception):
     def __init__(self, reason):
-        super(ComradeException, self).__init__()
+        super(ComradeError, self).__init__()
         self.reason = reason
         self.name = 'General Error'
 
@@ -24,14 +24,14 @@ class ComradeException(Exception):
     def __str__(self):
         return self.__unicode__()
 
-class BadRequestException(ComradeException):
+class BadRequestError(ComradeError):
     def __init__(self, reason):
-        super(BadRequestException, self).__init__(reason)
+        super(BadRequestError, self).__init__(reason)
         self.name = 'Bad Request'
 
-class HttpResponseException(ComradeException):
+class HttpResponseError(ComradeError):
     def __init__(self, reason, url, response):
-        super(HttpResponseException, self).__init__(reason)
+        super(HttpResponseError, self).__init__(reason)
         self.name = 'HTTP Exception'
         self.url = url
         self.response = response
