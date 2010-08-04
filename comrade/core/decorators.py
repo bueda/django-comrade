5f41695a0d1cbe50771101b94b87eeb08a3040aa
Christopher Peplin <peplin@bueda.com>
2010-08-04 16:22:21 -0400

    Match up decorator syntax/naming.

--- comrade/core/decorators.py
+++ comrade/core/decorators.py
@@ -20,21 +20,20 @@ def authorized(test_func, template_name='401.html'):
     callable that takes the user object and returns True if the user passes.
     """
 
-    def decorator(view_func):
-        def _wrapped_view(request, *args, **kwargs):
+    def decorator(view):
+        def _wrapped(request, *args, **kwargs):
             if test_func(request.user, *args, **kwargs):
-                return view_func(request, *args, **kwargs)
+                return view(request, *args, **kwargs)
             return direct_to_template(template_name, status=401)
-        return wraps(view_func,
-                assigned=available_attrs(view_func))(_wrapped_view)
+        return wraps(view, assigned=available_attrs(view))(_wrapped)
     return decorator
 
 def load_instance(model):
     def decorator(view):
-        def _wrapper(request, object_id=None, *args, **kwargs):
+        def _wrapped(request, object_id=None, *args, **kwargs):
             if object_id:
                 instance = get_object_or_404(model, pk=object_id)
                 return view(request, instance, *args, **kwargs)
             return view(request, *args, **kwargs)
-        return wraps(view)(_wrapper)
+        return wraps(view, assigned=available_attrs(view))(_wrapped)
     return decorator
