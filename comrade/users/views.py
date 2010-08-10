3218f511580dac3385b9792b1e36ebbd4e7cccf3
Christopher Peplin <peplin@bueda.com>
2010-08-10 09:44:09 -0400

    Add single-sign on (for Tender) support with custom login view.

---
+++ comrade/users/views.py
@@ -0,0 +1,67 @@
+from django.conf import settings
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+
+import re
+
+from comrade.users import REDIRECT_FIELD_NAME
+from comrade.users import utils
+from comrade.views.simple import direct_to_template
+
+import commonware.log
+logger = commonware.log.getLogger('comrade.users.views')
+
+def login(request, multipass=False, template_name='registration/login.html',
+          redirect_field_name=REDIRECT_FIELD_NAME,
+          authentication_form=AuthenticationForm):
+    """Displays the login form and handles the login action."""
+
+    if request.user.is_authenticated():
+        return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
+
+    redirect_to = request.REQUEST.get(redirect_field_name, '')
+
+    if hasattr(settings, 'TENDER_URL'):
+        tender_url = settings.TENDER_URL
+    else:
+        tender_url = None
+    
+    if request.method == "POST":
+        form = authentication_form(data=request.POST)
+        if form.is_valid():
+            if not redirect_to or ' ' in redirect_to:
+                redirect_to = settings.LOGIN_REDIRECT_URL
+            # Heavier security check -- redirects to http://example.com should 
+            # not be allowed, but things like /view/?param=http://example.com 
+            # should be allowed. This regex checks if there is a '//' *before* a
+            # question mark. Unless of course, the redirect is to the Tender
+            # app.
+            elif '//' in redirect_to and re.match(r'[^\?]*//', redirect_to):
+                if redirect_to != tender_url:
+                    redirect_to = settings.LOGIN_REDIRECT_URL
+            
+            auth_login(request, form.get_user())
+
+            if request.session.test_cookie_worked():
+                request.session.delete_test_cookie()
+
+            if multipass and redirect_to == tender_url:
+                redirect_to += '?sso=' + utils.multipass(request.user)
+
+            return HttpResponseRedirect(redirect_to)
+    else:
+        form = authentication_form(request)
+
+    if multipass:
+        login_url = reverse('account:multipass_login')
+    else:
+        login_url = reverse('account:login')
+    
+    request.session.set_test_cookie()
+    return direct_to_template(request, template_name, {
+        'form': form,
+        'redirect_field_name': redirect_field_name,
+        'redirect_to': redirect_to,
+        'login_url': login_url})
