7f41ea6734a79d7f46c214b98eb61f6d159da50f
Christopher Peplin <peplin@bueda.com>
2010-10-02 21:05:54 -0400

    This ain't fast, but it sure is purdy.

--- comrade/views/simple.py
+++ comrade/views/simple.py
@@ -2,8 +2,6 @@ from django.http import HttpResponse, HttpResponseServerError
 from django.template import RequestContext, loader
 from django.conf import settings
 
-from maintenancemode.http import HttpResponseTemporaryUnavailable
-
 import logging
 logger = logging.getLogger('comrade.views.simple')
 
@@ -16,6 +14,8 @@ def server_error(request, template_name='500.html'):
     return HttpResponseServerError(t.render(RequestContext(request)))
 
 def maintenance_mode(request, template_name='503.html'):
+    from maintenancemode.http import HttpResponseTemporaryUnavailable
+
     t = loader.get_template(template_name)
     return HttpResponseTemporaryUnavailable(t.render(RequestContext(request)))
 
