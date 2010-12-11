be8ad2a6165fd6229aa39033869754a75748b7de
Christopher Peplin <peplin@bueda.com>
2010-12-11 18:06:17 -0500

    Add AcceptMiddleware that parses the HTTP_ACCEPT header and stores it on request.accepted_types and request.accepts.

--- comrade/core/middleware.py
+++ comrade/core/middleware.py
@@ -124,3 +124,30 @@ class POSTDataMassageMiddleware(object):
                     request.data = request.POST
                 else:
                     request.data = request.PUT
+
+
+class AcceptMiddleware(object):
+    def _parse_accept_header(self, accept):
+        """Parse the Accept header *accept*, returning a list with pairs of
+        (media_type, q_value), ordered by q values.
+        """
+        result = []
+        for media_range in accept.split(","):
+            parts = media_range.split(";")
+            media_type = parts.pop(0)
+            media_params = []
+            q = 1.0
+            for part in parts:
+                (key, value) = part.lstrip().split("=", 1)
+                if key == "q":
+                    q = float(value)
+                else:
+                    media_params.append((key, value))
+            result.append((media_type, tuple(media_params), q))
+        result.sort(lambda x, y: -cmp(x[2], y[2]))
+        return result
+
+    def process_request(self, request):
+        accept = self._parse_accept_header(request.META.get("HTTP_ACCEPT", ""))
+        request.accept = accept
+        request.accepted_types = map(lambda (t, p, q): t, accept)
