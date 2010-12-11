d8c5b9dccb8e448fa1970687baf9ebe31187cbae
Christopher Peplin <peplin@bueda.com>
2010-12-10 22:32:56 -0500

    Accept kwargs in all render_to_response methods.

--- comrade/views/generic.py
+++ comrade/views/generic.py
@@ -8,9 +8,10 @@ from django.views.generic.detail import (BaseDetailView,
 from comrade.serializers import UniversalJSONSerializer
 
 class JSONResponseMixin(object):
-    def render_to_response(self, context):
+    def render_to_response(self, context, **httpresponse_kwargs):
         """Returns a JSON response containing 'context' as payload."""
-        return self.get_json_response(self.convert_context_to_json(context))
+        return self.get_json_response(self.convert_context_to_json(context),
+                **httpresponse_kwargs)
 
     def get_json_response(self, content, **httpresponse_kwargs):
         """Construct an `HttpResponse` object."""
@@ -30,20 +31,22 @@ class JSONDetailView(JSONResponseMixin, BaseDetailView):
 
 class HybridDetailView(JSONResponseMixin, SingleObjectTemplateResponseMixin,
         BaseDetailView):
-    def render_to_response(self, context):
+    def render_to_response(self, context, **response_kwargs):
         # Look for a 'format=json' GET argument
         if self.request.is_ajax() or self.request.GET.get('format') == 'json':
-            return JSONResponseMixin.render_to_response(self, context)
+            return JSONResponseMixin.render_to_response(self, context,
+                    **response_kwargs)
         else:
             return SingleObjectTemplateResponseMixin.render_to_response(self,
-                    context)
+                    context, **response_kwargs)
 
 class HybridListView(JSONResponseMixin, MultipleObjectTemplateResponseMixin,
         BaseListView):
-    def render_to_response(self, context):
+    def render_to_response(self, context, **response_kwargs):
         # Look for a 'format=json' GET argument
         if self.request.is_ajax() or self.request.GET.get('format') == 'json':
-            return JSONResponseMixin.render_to_response(self, context)
+            return JSONResponseMixin.render_to_response(self, context,
+                    **response_kwargs)
         else:
-            return MultiObjectTemplateResponseMixin.render_to_response(self,
-                    context)
+            return MultipleObjectTemplateResponseMixin.render_to_response(self,
+                    context, **response_kwargs)
