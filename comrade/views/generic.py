5c77b1b6440054572903464fb027ee4c92b8252b
Christopher Peplin <peplin@bueda.com>
2010-12-11 18:06:39 -0500

    Generalize content negotiation in a ContentNegotiationMixin.

--- comrade/views/generic.py
+++ comrade/views/generic.py
@@ -1,54 +1,81 @@
-from django import http
+from django.http import HttpResponse
 from django.core import serializers
 from django.views.generic.list import (BaseListView,
         MultipleObjectTemplateResponseMixin)
 from django.views.generic.detail import (BaseDetailView,
         SingleObjectTemplateResponseMixin)
 
-from comrade.serializers import UniversalJSONSerializer
-
-class JSONResponseMixin(object):
-    def render_to_response(self, context, **httpresponse_kwargs):
-        """Returns a JSON response containing 'context' as payload."""
-        return self.get_json_response(self.convert_context_to_json(context),
-                **httpresponse_kwargs)
-
-    def get_json_response(self, content, **httpresponse_kwargs):
-        """Construct an `HttpResponse` object."""
-        return http.HttpResponse(content,
-                                 content_type='application/json',
-                                 **httpresponse_kwargs)
-
-    def convert_context_to_json(self, context):
-        """Convert the context dictionary into a JSON object"""
-        return UniversalJSONSerializer().serialize(context, ensure_ascii=False)
-
-class JSONListView(JSONResponseMixin, BaseListView):
+try:
+    # Import Piston if it's installed, but don't die if it's not here. Only a
+    # limited number of Middleware require it.
+    import piston.utils
+    
+    # Importing this registers translators for the MimeTypes we are using.
+    import piston.emitters
+except ImportError:
     pass
+else:
+    piston.emitters.Emitter.register('text/xml', piston.emitters.XMLEmitter,
+            'text/xml; charset=utf-8')
+    piston.emitters.Emitter.register('application/json',
+            piston.emitters.JSONEmitter, 'application/json; charset=utf-8')
 
-class JSONDetailView(JSONResponseMixin, BaseDetailView):
-    pass
+class ContentNegotiationMixin(object):
+    """Requires the AcceptMiddleware to be enabled."""
+    def _determine_accepted_types(self, request):
+        type_html = "text/html"
+        if type_html in request.accepted_types:
+            return []
+        return request.accepted_types
+        
+    def get_api_response(self, context, **kwargs):
+        """If the HttpRequest has something besides text/html in its ACCEPT
+        header, try and serialize the context and return an HttpResponse.
 
-class HybridDetailView(JSONResponseMixin, SingleObjectTemplateResponseMixin,
-        BaseDetailView):
-    def render_to_response(self, context, **response_kwargs):
-        # Look for a 'format=json' GET argument
-        if self.request.is_ajax() or self.request.GET.get('format') == 'json':
-            return JSONResponseMixin.render_to_response(self, context,
-                    **response_kwargs)
-        else:
-            return SingleObjectTemplateResponseMixin.render_to_response(self,
-                    context, **response_kwargs)
+        This also responds to a ?format=x query parameter.
 
-class HybridListView(JSONResponseMixin, MultipleObjectTemplateResponseMixin,
-        BaseListView):
-    def render_to_response(self, context, **response_kwargs):
+        Uses Piston's Emitter utility, so Piston is a requirement for now.
+        """
         # Look for a 'format=json' GET argument or CONTENT-TYPE of
         # application/json
-        if (self.request.GET.get('format') == 'json' or
-                self.request.META.get('CONTENT_TYPE') == 'application/json'):
-            return JSONResponseMixin.render_to_response(self, context,
-                    **response_kwargs)
-        else:
-            return MultipleObjectTemplateResponseMixin.render_to_response(self,
-                    context, **response_kwargs)
+        accepted_types = self._determine_accepted_types(self.request)
+        for accepted_type in accepted_types:
+            try:
+                emitter, ct = piston.emitters.Emitter.get(accepted_type)
+            except ValueError:
+                pass
+            else:
+                srl = emitter(context, {}, None)
+                try:
+                    stream = srl.render(self.request)
+                    if not isinstance(stream, HttpResponse):
+                        response = HttpResponse(stream, mimetype=ct, **kwargs)
+                    else:
+                        response = stream
+                    return response
+                except piston.utils.HttpStatusCode, e:
+                    return e.response
+
+class HybridDetailView(ContentNegotiationMixin,
+        SingleObjectTemplateResponseMixin, BaseDetailView):
+    """Return an object detail view, either HTML, JSON or XML (depending on the
+    ACCEPT HTTP header or ?format=x query parameter.
+    """
+    def render_to_response(self, context, **kwargs):
+        response = self.get_api_response(context, **kwargs)
+        if not response:
+            response = SingleObjectTemplateResponseMixin.render_to_response(
+                    self, context, **kwargs)
+        return response
+
+class HybridListView(ContentNegotiationMixin,
+        MultipleObjectTemplateResponseMixin, BaseListView):
+    """Return an object list view, either HTML, JSON or XML (depending on the
+    ACCEPT HTTP header or ?format=x query parameter.
+    """
+    def render_to_response(self, context, **kwargs):
+        response = self.get_api_response(context, **kwargs)
+        if not response:
+            response = MultipleObjectTemplateResponseMixin.render_to_response(
+                    self, context, **kwargs)
+        return response
