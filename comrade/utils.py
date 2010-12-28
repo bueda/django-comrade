b800f746e73634fe04a9a1ec45ef62dd7528f219
Christopher Peplin <peplin@bueda.com>
2010-12-27 20:41:47 -0500

    Return lowercased random tokens for consistency.

--- comrade/utils.py
+++ comrade/utils.py
@@ -6,7 +6,7 @@ def generate_key():
     key = hashlib.sha224(str(random.getrandbits(256))).digest()
     key = base64.b64encode(key, 
             random.choice(['rA','aZ','gQ','hH','hG','aR','DD']))
-    key = key.rstrip('==')
+    key = key.rstrip('==').lower()
     return key
 
 def chunked(seq, n):
