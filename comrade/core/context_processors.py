559fa4bf1982de6dd4a8943939b535972731bd08
Christopher Peplin <peplin@bueda.com>
2011-01-05 15:44:45 -0500

    Add full base URL for site to default context.

--- comrade/core/context_processors.py
+++ comrade/core/context_processors.py
@@ -5,10 +5,16 @@ from settings import DeploymentType
 def default(request):
     context = {}
     context['DEPLOYMENT'] = settings.DEPLOYMENT
-    context['site'] = Site.objects.get_current()
+    context['current_site'] = Site.objects.get_current()
     if settings.DEPLOYMENT != DeploymentType.PRODUCTION:
         context['GIT_COMMIT'] = settings.GIT_COMMIT
     context['site_email'] = settings.CONTACT_EMAIL
+    if request.is_secure():
+        context['protocol'] = 'https://'
+    else:
+        context['protocol'] = 'http://'
+    context['current_site_url'] = (context['protocol'] +
+            context['current_site'].domain)
     return context
 
 def ssl_media(request):
