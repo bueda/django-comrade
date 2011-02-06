3ada5a7ebfe73f55b6d247ba016829cf8a0b5d4a
Christopher Peplin <peplin@bueda.com>
2011-02-05 20:22:46 -0500

    Use string.strip() instead of starting regexps with \n.

--- comrade/mail/mail.py
+++ comrade/mail/mail.py
@@ -32,36 +32,36 @@ def direct_to_email(template, recipient_list, extra_context=None,
 # http://stackoverflow.com/questions/1372694/strip-signatures-and-replies-from-emails/2193937#2193937
 EMAIL_SIGNATURES = (
         # standard
-        re.compile(r'\n-- \n$', flags=re.MULTILINE), 
+        re.compile(r'^-- \n$', flags=re.MULTILINE), 
         # standard missing space
-        re.compile(r'\n--\n$', flags=re.MULTILINE), 
+        re.compile(r'^--\n$', flags=re.MULTILINE), 
         # ego-booster
-        re.compile(r'\nSent from my', flags=re.MULTILINE),
+        re.compile(r'^Sent from my', flags=re.MULTILINE),
 )
 
 def strip_signatures(body):
     for signature in EMAIL_SIGNATURES:
         split_signature = signature.split(body)
         if len(split_signature) > 1:
-            return split_signature[0]
+            return split_signature[0].strip()
     return body
 
 EMAIL_QUOTED_MARKERS = (
         # outlook
-        re.compile(r'\n-----Original Message-----', flags=re.MULTILINE),
+        re.compile(r'^-----Original Message-----', flags=re.MULTILINE),
         # outlook alternative
-        re.compile(r'\n________________________________', flags=re.MULTILINE),
+        re.compile(r'^________________________________', flags=re.MULTILINE),
         # mail.app
-        re.compile(r'\nOn [\w: ]+ wrote:\n$', flags=re.MULTILINE),
+        re.compile(r'^On .+ wrote:\n$', flags=re.MULTILINE),
         # failsafe
-        re.compile(r'\nFrom: ', flags=re.MULTILINE),
+        re.compile(r'^From: ', flags=re.MULTILINE),
         # indented
-        re.compile(r'\n>.*$', flags=re.MULTILINE),
+        re.compile(r'^>.*$', flags=re.MULTILINE),
 )
 
 def strip_quoted(body):
     for quoted in EMAIL_QUOTED_MARKERS:
         split_message = quoted.split(body)
         if len(split_message) > 1:
-            return split_message[0]
+            return split_message[0].strip()
     return body
