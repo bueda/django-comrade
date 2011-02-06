254dc772350523408eb1132fe11b2bcbabd78e74
Christopher Peplin <peplin@bueda.com>
2011-02-05 20:09:08 -0500

    Split up signature and inline quoting e-mail regexes.

--- comrade/mail/mail.py
+++ comrade/mail/mail.py
@@ -32,19 +32,11 @@ def direct_to_email(template, recipient_list, extra_context=None,
 # http://stackoverflow.com/questions/1372694/strip-signatures-and-replies-from-emails/2193937#2193937
 EMAIL_SIGNATURES = (
         # standard
-        re.compile(r'^-- \n$', flags=re.MULTILINE), 
+        re.compile(r'\n-- \n$', flags=re.MULTILINE), 
         # standard missing space
-        re.compile(r'^--\n$', flags=re.MULTILINE), 
-        # outlook
-        re.compile("^-----Original Message-----", flags=re.MULTILINE),
-        # outlook alternative
-        re.compile("^________________________________", flags=re.MULTILINE),
-        # mail.app
-        re.compile("^On [\w: ]+ wrote:\n$", flags=re.MULTILINE),
-        # failsafe
-        re.compile("^From: ", flags=re.MULTILINE),
+        re.compile(r'\n--\n$', flags=re.MULTILINE), 
         # ego-booster
-        re.compile("^Sent from my", flags=re.MULTILINE),
+        re.compile(r'\nSent from my', flags=re.MULTILINE),
 )
 
 def strip_signatures(body):
@@ -54,3 +46,22 @@ def strip_signatures(body):
             return split_signature[0]
     return body
 
+EMAIL_QUOTED_MARKERS = (
+        # outlook
+        re.compile(r'\n-----Original Message-----', flags=re.MULTILINE),
+        # outlook alternative
+        re.compile(r'\n________________________________', flags=re.MULTILINE),
+        # mail.app
+        re.compile(r'\nOn [\w: ]+ wrote:\n$', flags=re.MULTILINE),
+        # failsafe
+        re.compile(r'\nFrom: ', flags=re.MULTILINE),
+        # indented
+        re.compile(r'\n>.*$', flags=re.MULTILINE),
+)
+
+def strip_quoted(body):
+    for quoted in EMAIL_QUOTED_MARKERS:
+        split_message = quoted.split(body)
+        if len(split_message) > 1:
+            return split_message[0]
+    return body
