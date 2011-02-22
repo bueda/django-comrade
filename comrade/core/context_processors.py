3770095f087309efe901c2f22afd29ba6f3ddd18
Christopher Peplin <peplin@bueda.com>
2011-02-21 21:23:48 -0500

    Add a context processor that adds the UserProfile to each context.

--- comrade/core/context_processors.py
+++ comrade/core/context_processors.py
@@ -17,6 +17,12 @@ def default(request):
             context['current_site'].domain)
     return context
 
+def profile(request):
+    context = {}
+    if request.user.is_authenticated():
+        context['profile'] = request.user.get_profile()
+    return context
+
 def ssl_media(request):
     if request.is_secure():
         ssl_media_url = settings.MEDIA_URL.replace('http://','https://')
