2374b546a581cf1d299d35a9192091082f5c0d29
Christopher Peplin <peplin@bueda.com>
2011-05-25 19:51:45 -0400

    Document partial template mixin.

--- comrade/views/generic/partial.py
+++ comrade/views/generic/partial.py
@@ -8,6 +8,13 @@ class PartialMixin(object):
 
 
 class PartialTemplateResponseMixin(TemplateResponseMixin, PartialMixin):
+    """Return a Rails-style parital HTML fragment if ?format=partial.
+
+    This mixin requires that you specify an additional Django template beyond
+    the one for normal requests, this one a smaller fragment or partial. The
+    mixin is useful if you have a complicated bit of HTML that you are loading
+    via an AJAX call, but don't want to construct in JavaScript.
+    """
     partial_template_name = None
 
     def get_template_names(self):
